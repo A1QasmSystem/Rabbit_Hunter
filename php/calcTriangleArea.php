1efe2733f9ac6edfb3ea85f9dc1f9fc5407b0607
David Ismail <69373282+sahabatdavid@users.noreply.github.com>
2021-10-25 04:45:40 -0700

    Create calcTriangleArea.php

---
+++ php/calcTriangleArea.php
@@ -0,0 +1,11 @@
+<?php
+
+class Calculate {
+  public function calcTriangleArea($alas, $tinggi) {
+    $rectanlgeArea = 0.5 * $alas * $tinggi;
+    return $rectangleArea;
+  }
+}
+
+$tri = new calcTriangleArea;
+echo $tri->calcTriangleArea(5,3);
